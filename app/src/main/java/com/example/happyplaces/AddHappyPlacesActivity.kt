66d7010084902f3acbce6542a32b3554bf0537d9
parfenovedyard <parfenovedyard@gmail.com>
2021-06-01 17:21:29 +0300

    add strings, colors, dimens to "added places" activity color corrections to theme

--- app/src/main/java/com/example/happyplaces/AddHappyPlacesActivity.kt
+++ app/src/main/java/com/example/happyplaces/AddHappyPlacesActivity.kt
@@ -2,10 +2,21 @@ package com.example.happyplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.happyplaces.databinding.ActivityAddHappyPlacesBinding
 
 class AddHappyPlacesActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddHappyPlacesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_happy_places)
+        binding = ActivityAddHappyPlacesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.tbAddPlace)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.tbAddPlace.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
--- app/src/main/java/com/example/happyplaces/MainActivity.kt
+++ app/src/main/java/com/example/happyplaces/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.happyplaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.happyplaces.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fabAddHappyPlace.setOnClickListener {
+            val intent = Intent(this, AddHappyPlacesActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
