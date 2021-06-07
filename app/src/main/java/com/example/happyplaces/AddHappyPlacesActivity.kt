2ba7ab1f509f57061756bea61030c0ba75bd0b5e
parfenovedyard <parfenovedyard@gmail.com>
2021-06-07 15:32:48 +0300

    add calendar picker

--- app/src/main/java/com/example/happyplaces/AddHappyPlacesActivity.kt
+++ app/src/main/java/com/example/happyplaces/AddHappyPlacesActivity.kt
@@ -1,12 +1,18 @@
 package com.example.happyplaces
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.happyplaces.databinding.ActivityAddHappyPlacesBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddHappyPlacesActivity : AppCompatActivity() {
+class AddHappyPlacesActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityAddHappyPlacesBinding
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +24,34 @@ class AddHappyPlacesActivity : AppCompatActivity() {
         binding.tbAddPlace.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        dateSetListener = DatePickerDialog.OnDateSetListener {
+                view, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            updateDateInView()
+        }
+        binding.etDate.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.et_date -> {
+                DatePickerDialog(
+                    this@AddHappyPlacesActivity,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
+        }
+    }
+
+    private fun updateDateInView() {
+        val myFormat = "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+        binding.etDate.setText(sdf.format(cal.time).toString())
     }
 }
