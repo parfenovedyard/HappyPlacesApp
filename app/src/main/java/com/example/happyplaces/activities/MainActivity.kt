c458c3e61efb4c3ae5f13db692cc7e6fa70dfc7e
parfenovedyard <parfenovedyard@gmail.com>
2021-06-08 18:25:06 +0300

    reading data from local DB

--- app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.happyplaces.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.happyplaces.database.DatabaseHandler
 import com.example.happyplaces.databinding.ActivityMainBinding
+import com.example.happyplaces.models.HappyPlaceModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,5 +21,20 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AddHappyPlacesActivity::class.java)
             startActivity(intent)
         }
+        getHappyPlacesListFromDB()
     }
+
+    private fun getHappyPlacesListFromDB() {
+        val dbHandler = DatabaseHandler(this)
+        val getHappyPlaceList : ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
+
+        if (getHappyPlaceList.size > 0) {
+            for (i in getHappyPlaceList) {
+                Log.e("title", i.title)
+                Log.e("description", i.description)
+            }
+        }
+
+    }
+
 }
