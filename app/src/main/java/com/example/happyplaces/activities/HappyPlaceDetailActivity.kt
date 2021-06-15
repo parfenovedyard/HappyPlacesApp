17a38208c6024599a2904f36bfb1abd330c99972
parfenovedyard <parfenovedyard@gmail.com>
2021-06-15 23:13:38 +0300

    passing activity from one activity to another

--- app/src/main/java/com/example/happyplaces/activities/HappyPlaceDetailActivity.kt
+++ app/src/main/java/com/example/happyplaces/activities/HappyPlaceDetailActivity.kt
@@ -1,12 +1,36 @@
 package com.example.happyplaces.activities
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.happyplaces.R
+import com.example.happyplaces.databinding.ActivityHappyPlaceDetailBinding
+import com.example.happyplaces.models.HappyPlaceModel
 
 class HappyPlaceDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding :ActivityHappyPlaceDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_happy_place_detail)
+        binding = ActivityHappyPlaceDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var happyPlaceDetailModel: HappyPlaceModel? = null
+        if (intent.hasExtra(MainActivity.EXTRA_PLACE_DETAILS)) {
+            happyPlaceDetailModel = intent.getSerializableExtra(MainActivity.EXTRA_PLACE_DETAILS) as HappyPlaceModel
+        }
+
+        if (happyPlaceDetailModel != null) {
+            setSupportActionBar(binding.tbHappyPlaceDetail)
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+            supportActionBar!!.title = happyPlaceDetailModel.title
+
+            binding.tbHappyPlaceDetail.setNavigationOnClickListener {
+                onBackPressed()
+            }
+            binding.ivPlaceImage.setImageURI(Uri.parse(happyPlaceDetailModel.image))
+            binding.tvDescription.text = happyPlaceDetailModel.description
+            binding.tvLocation.text = happyPlaceDetailModel.location
+        }
     }
 }
