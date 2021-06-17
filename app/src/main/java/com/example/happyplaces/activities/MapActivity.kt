ab884ac0481c77d00735e862ca6f68a167648fd9
parfenovedyard <parfenovedyard@gmail.com>
2021-06-18 00:38:47 +0300

    Displaying A Marker On The Map And Zooming In With An Animation

--- app/src/main/java/com/example/happyplaces/activities/MapActivity.kt
+++ app/src/main/java/com/example/happyplaces/activities/MapActivity.kt
@@ -2,11 +2,51 @@ package com.example.happyplaces.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 import com.example.happyplaces.R
+import com.example.happyplaces.databinding.ActivityMapBinding
+import com.example.happyplaces.models.HappyPlaceModel
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var binding: ActivityMapBinding
+    private var mHappyPlaceDetail: HappyPlaceModel? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
+        binding = ActivityMapBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (intent.hasExtra(MainActivity.EXTRA_PLACE_DETAILS)) {
+            mHappyPlaceDetail = intent.getSerializableExtra(MainActivity.
+            EXTRA_PLACE_DETAILS) as HappyPlaceModel
+        }
+        if (mHappyPlaceDetail != null) {
+            setSupportActionBar(binding.tbMap)
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+            supportActionBar!!.title = mHappyPlaceDetail!!.title
+
+            binding.tbMap.setNavigationOnClickListener {
+                onBackPressed()
+            }
+        }
+
+        val supportMapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        supportMapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+        val position = LatLng(mHappyPlaceDetail!!.latitude, mHappyPlaceDetail!!.longitude)
+        p0!!.addMarker(MarkerOptions().position(position).title(mHappyPlaceDetail!!.location))
+        val newLatLngZoom = CameraUpdateFactory.newLatLngZoom(position, 15f)
+        p0.animateCamera(newLatLngZoom)
     }
 }
